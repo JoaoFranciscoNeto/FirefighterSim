9fb95689cc66d5aa58a85b0b6133a6d6348daa9a
João Neto <jneto@fe.up.pt>
2018-03-22 15:39:57 +0000

    Propagating fire according to simple model
    Signed-off-by: João Neto <jneto@fe.up.pt>

--- Assets/FireModelling/Grid.cs
+++ Assets/FireModelling/Grid.cs
@@ -16,31 +16,39 @@ namespace FireModelling
         // Use this for initialization
         void Start()
         {
-            cellGrid = new Cell[10, 10];
+            cellGrid = new Cell[100, 100];
 
-            for (int x = 0; x < 10; x++)
+            for (int x = 0; x < 100; x++)
             {
-                for (int y = 0; y < 10; y++)
+                for (int y = 0; y < 100; y++)
                 {
                     cellGrid[x, y] = new Cell(this, new Vector2(x, y));
                 }
             }
 
-            cellGrid[Random.Range(0, 10), Random.Range(0, 10)].state = Cell.State.OnFire;
+            cellGrid[Random.Range(0, 100), Random.Range(0, 100)].state = Cell.State.OnFire;
         }
+
+        float timeToStep = 1.0f;
 
         // Update is called once per frame
         void Update()
         {
-            SimulationTick();
+            timeToStep -= Time.deltaTime;
+            if (timeToStep<=0)
+            {
+
+                SimulationTick();
+                timeToStep = 1.0f;
+            }
         }
 
         void SimulationTick()
         {
-            Cell[,] newGrid = new Cell[10, 10];
-            for (int x = 0; x < 10; x++)
+            Cell[,] newGrid = new Cell[100, 100];
+            for (int x = 0; x < 100; x++)
             {
-                for (int y = 0; y < 10; y++)
+                for (int y = 0; y < 100; y++)
                 {
                     newGrid[x, y] = cellGrid[x, y].SimulationTick();
                 }
@@ -79,6 +87,13 @@ namespace FireModelling
             public Vector2 Wind;
             public float Fuel;
 
+            public Info(float Temperature, float IgnPoint, float Fuel)
+            {
+                this.Temperature = Temperature;
+                this.IgnPoint = IgnPoint;
+                this.Wind = new Vector2(.1f, 0);
+                this.Fuel = Fuel;
+            }
             public Info(float Temperature, float IgnPoint, Vector2 Wind, float Fuel)
             {
                 this.Temperature = Temperature;
@@ -86,15 +101,22 @@ namespace FireModelling
                 this.Wind = Wind;
                 this.Fuel = Fuel;
             }
-            
+
         }
 
-        Info info;
-        Info newInfo;
+        public Info info;
 
         public State state;
         public Vector2 position;
 
+
+        public Cell(Cell current, Info newInfo)
+        {
+            this.grid = current.grid;
+            this.info = newInfo;
+            this.position = current.position;
+            this.state = current.state;
+        }
 
 
         public Cell(Grid parent, Vector2 pos)
@@ -102,17 +124,47 @@ namespace FireModelling
             this.grid = parent;
             this.position = pos;
             this.state = State.Clear;
+            this.info = new Info(30, 60, Random.Range(0f,1f));
         }
 
         public Cell SimulationTick()
         {
-            Cell newMe = null;
-            foreach (Cell cell in GetNeighbours())
-            {
+            Info newInfo = new Info(info.Temperature,info.IgnPoint,info.Fuel);
 
+            switch (state)
+            {
+                case State.Clear:
+
+                    foreach (Cell neighbour in GetNeighbours())
+                    {
+                        Vector2 dir = neighbour.position - position;
+                        float windStrength = Vector2.Dot(dir, info.Wind);
+                        newInfo.Temperature += ((neighbour.info.Temperature - info.Temperature) / 2f);
+                    }
+
+                    if (newInfo.Temperature > info.IgnPoint)
+                    {
+                        state = State.OnFire;
+                    }
+
+                    break;
+                case State.OnFire:
+
+                    // Burn fuel
+                    newInfo.Fuel -= .05f;
+                    newInfo.Temperature += 50f;
+                    // Check if there is fuel left
+                    if (newInfo.Fuel <= 0)
+                    {
+                        newInfo.Fuel = 0;
+                        state = State.Burnt;
+                    }
+
+                    break;
             }
 
-            return newMe;
+
+            return new Cell(this, newInfo);
         }
         
 
@@ -137,11 +189,11 @@ namespace FireModelling
 
             if (position.x > 0)
                 neighbours.Add(grid.cellGrid[(int)position.x - 1, (int)position.y]);
-            if (position.x < grid.cellGrid.GetLength(0))
+            if (position.x < grid.cellGrid.GetLength(0)-1)
                 neighbours.Add(grid.cellGrid[(int)position.x + 1, (int)position.y]);
             if (position.y > 0)
                 neighbours.Add(grid.cellGrid[(int)position.x, (int)position.y - 1]);
-            if (position.y < grid.cellGrid.GetLength(1))
+            if (position.y < grid.cellGrid.GetLength(1)-1)
                 neighbours.Add(grid.cellGrid[(int)position.x, (int)position.y + 1]);
 
             return neighbours.ToArray();

fee6ebf0c5f0474a17d792e1ac6d641198046521
João Neto <jneto@fe.up.pt>
2018-03-23 18:57:12 +0000

    Refactor of the automata structure
    Signed-off-by: João Neto <jneto@fe.up.pt>

--- Assets/FireModelling/Grid.cs
+++ Assets/FireModelling/Grid.cs
@@ -10,23 +10,29 @@ namespace FireModelling
 
     public class Grid : MonoBehaviour
     {
-        public Cell[,] cellGrid;
 
+        public GameObject cellPrefab;
+
+        public GridCell[,] cellGrid;
+
+        public int VisualizationMode = 0;
 
         // Use this for initialization
         void Start()
         {
-            cellGrid = new Cell[100, 100];
+            cellGrid = new GridCell[100, 100];
 
             for (int x = 0; x < 100; x++)
             {
                 for (int y = 0; y < 100; y++)
                 {
-                    cellGrid[x, y] = new Cell(this, new Vector2(x, y));
+                    
+                    cellGrid[x, y] = Instantiate(cellPrefab, new Vector3(x, 0, y), Quaternion.identity, transform).GetComponent<GridCell>();
+                    cellGrid[x, y].grid = this;
                 }
             }
 
-            cellGrid[Random.Range(0, 100), Random.Range(0, 100)].state = Cell.State.OnFire;
+            cellGrid[Random.Range(0, 100), Random.Range(0, 100)].state = GridCell.State.OnFire;
         }
 
         float timeToStep = 1.0f;
@@ -45,7 +51,7 @@ namespace FireModelling
 
         void SimulationTick()
         {
-            Cell[,] newGrid = new Cell[100, 100];
+            GridCell[,] newGrid = new GridCell[100, 100];
             for (int x = 0; x < 100; x++)
             {
                 for (int y = 0; y < 100; y++)
@@ -53,157 +59,17 @@ namespace FireModelling
                     newGrid[x, y] = cellGrid[x, y].SimulationTick();
                 }
             }
-            cellGrid = newGrid.Clone() as Cell[,];
+            cellGrid = newGrid.Clone() as GridCell[,];
         }
 
-        private void OnDrawGizmos()
+
+
+        public void OnVisualizationChange(int newState)
         {
-            foreach (Cell cell in cellGrid)
-            {
-                Gizmos.color = cell.DebugColor();
-                Gizmos.DrawCube(new Vector3(cell.position.x, 0, cell.position.y), Vector3.one*.9f);
-            }
+            Debug.Log("Changed visualization to " + newState);
+            VisualizationMode = newState;
         }
-
 
     }
-
-
-    public class Cell
-    {
-        Grid grid;
-
-        public enum State
-        {
-            Clear,
-            OnFire,
-            Burnt
-        }
-
-        public class Info
-        {
-            public float Temperature;
-            public float IgnPoint;
-            public Vector2 Wind;
-            public float Fuel;
-
-            public Info(float Temperature, float IgnPoint, float Fuel)
-            {
-                this.Temperature = Temperature;
-                this.IgnPoint = IgnPoint;
-                this.Wind = new Vector2(.1f, 0);
-                this.Fuel = Fuel;
-            }
-            public Info(float Temperature, float IgnPoint, Vector2 Wind, float Fuel)
-            {
-                this.Temperature = Temperature;
-                this.IgnPoint = IgnPoint;
-                this.Wind = Wind;
-                this.Fuel = Fuel;
-            }
-
-        }
-
-        public Info info;
-
-        public State state;
-        public Vector2 position;
-
-
-        public Cell(Cell current, Info newInfo)
-        {
-            this.grid = current.grid;
-            this.info = newInfo;
-            this.position = current.position;
-            this.state = current.state;
-        }
-
-
-        public Cell(Grid parent, Vector2 pos)
-        {
-            this.grid = parent;
-            this.position = pos;
-            this.state = State.Clear;
-            this.info = new Info(30, 60, Random.Range(0f,1f));
-        }
-
-        public Cell SimulationTick()
-        {
-            Info newInfo = new Info(info.Temperature,info.IgnPoint,info.Fuel);
-
-            switch (state)
-            {
-                case State.Clear:
-
-                    foreach (Cell neighbour in GetNeighbours())
-                    {
-                        Vector2 dir = neighbour.position - position;
-                        float windStrength = Vector2.Dot(dir, info.Wind);
-                        newInfo.Temperature += ((neighbour.info.Temperature - info.Temperature) / 2f);
-                    }
-
-                    if (newInfo.Temperature > info.IgnPoint)
-                    {
-                        state = State.OnFire;
-                    }
-
-                    break;
-                case State.OnFire:
-
-                    // Burn fuel
-                    newInfo.Fuel -= .05f;
-                    newInfo.Temperature += 50f;
-                    // Check if there is fuel left
-                    if (newInfo.Fuel <= 0)
-                    {
-                        newInfo.Fuel = 0;
-                        state = State.Burnt;
-                    }
-
-                    break;
-            }
-
-
-            return new Cell(this, newInfo);
-        }
-        
-
-        public Color DebugColor()
-        {
-            switch (state)
-            {
-                case State.Clear:
-                    return Color.Lerp(Color.green, new Color(0, .5f, 0), info.Fuel);
-                case State.OnFire:
-                    return Color.red;
-                case State.Burnt:
-                    return Color.black;
-                default:
-                    return Color.cyan;
-            }
-        }
-
-        public Cell[] GetNeighbours()
-        {
-            List<Cell> neighbours = new List<Cell>();
-
-            if (position.x > 0)
-                neighbours.Add(grid.cellGrid[(int)position.x - 1, (int)position.y]);
-            if (position.x < grid.cellGrid.GetLength(0)-1)
-                neighbours.Add(grid.cellGrid[(int)position.x + 1, (int)position.y]);
-            if (position.y > 0)
-                neighbours.Add(grid.cellGrid[(int)position.x, (int)position.y - 1]);
-            if (position.y < grid.cellGrid.GetLength(1)-1)
-                neighbours.Add(grid.cellGrid[(int)position.x, (int)position.y + 1]);
-
-            return neighbours.ToArray();
-        }
-
-
-        public static T Clone<T>(T source)
-        {
-            var serialized = JsonConvert.SerializeObject(source);
-            return JsonConvert.DeserializeObject<T>(serialized);
-        }
-    }
+    
 }
---
+++ Assets/FireModelling/GridCell.cs
@@ -0,0 +1,147 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FireModelling
+{
+
+    public class GridCell : MonoBehaviour
+    {
+
+        public Grid grid;
+
+        public enum State
+        {
+            Clear,
+            OnFire,
+            Burnt
+        }
+
+        public class Info
+        {
+            public float Temperature;
+            public float IgnPoint;
+            public float Fuel;
+            
+
+        }
+
+        public State state;
+        public Info cellInfo;
+        Info newInfo;
+        public Vector2 position;
+
+        private void Awake()
+        {
+            this.state = State.Clear;
+            cellInfo = new Info();
+
+            cellInfo.Temperature = 28;
+            cellInfo.IgnPoint = 100;
+            cellInfo.Fuel = 200;
+        }
+        
+        GridCell[] GetNeighbours()
+        {
+            List<GridCell> neighbours = new List<GridCell>();
+
+            if (position.x > 0)
+                neighbours.Add(grid.cellGrid[(int)position.x - 1, (int)position.y]);
+            if (position.x < grid.cellGrid.GetLength(0) - 1)
+                neighbours.Add(grid.cellGrid[(int)position.x + 1, (int)position.y]);
+            if (position.y > 0)
+                neighbours.Add(grid.cellGrid[(int)position.x, (int)position.y - 1]);
+            if (position.y < grid.cellGrid.GetLength(1) - 1)
+                neighbours.Add(grid.cellGrid[(int)position.x, (int)position.y + 1]);
+
+            return neighbours.ToArray();
+        }
+
+        public GridCell SimulationTick()
+        {
+            newInfo = Clone(cellInfo);
+
+            newInfo.Temperature = UpdateTemperature();
+
+            switch (state)
+            {
+                case State.Clear:
+                    if (newInfo.Temperature >= newInfo.IgnPoint)
+                        state = State.OnFire;
+                    break;
+                case State.OnFire:
+                    newInfo.Temperature += 50;
+
+                    newInfo.Fuel -= 10;
+                    if (newInfo.Fuel <= 0)
+                    {
+                        newInfo.Fuel = 0;
+                        state = State.Burnt;
+                    }
+                    break;
+            }
+
+
+            return this;
+        }
+
+        public static float StefanBoltzman = 5.6703e-8f;
+
+        float UpdateTemperature()
+        {
+            float temp = cellInfo.Temperature;
+            foreach (GridCell neighbour in GetNeighbours())
+            {
+                temp += StefanBoltzman * (Mathf.Pow(neighbour.cellInfo.Temperature, 4) - Mathf.Pow(cellInfo.Temperature, 4));
+            }
+            Debug.Log(temp);
+            return temp;
+        }
+
+        public void UpdateInfo()
+        {
+            cellInfo = Clone(newInfo);
+        }
+
+        private void OnDrawGizmos()
+        {
+            switch(grid.VisualizationMode)
+            {
+                case 0:
+                    switch (state)
+                    {
+                        case State.Clear:
+                            Gizmos.color = Color.green;
+                            break;
+                        case State.Burnt:
+                            Gizmos.color = Color.black;
+                            break;
+                        case State.OnFire:
+                            Gizmos.color = Color.red;
+                            break;
+                    }
+                    break;
+                case 1:
+                    Gizmos.color = Color.Lerp(Color.green, Color.red, cellInfo.Temperature / 100f);
+                    break;
+
+
+            }
+
+            Gizmos.DrawCube(transform.position, Vector3.one * .95f);
+        }
+
+
+        public static T Clone<T>(T source)
+        {
+            var serialized = JsonConvert.SerializeObject(source);
+            return JsonConvert.DeserializeObject<T>(serialized);
+        }
+
+        public float CelsiusToKelvin(float celsius)
+        {
+            return celsius + 273.15f;
+        }
+    }
+}
